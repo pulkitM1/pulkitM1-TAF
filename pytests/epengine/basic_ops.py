107537f42b2a0c15ef5f6306db83996268a839ba
pulkitM1 <pulkit.matta@couchbase.com>
2022-07-08 12:58:24 +0530

    commit

--- pytests/epengine/basic_ops.py
+++ pytests/epengine/basic_ops.py
@@ -1388,8 +1388,8 @@ class basic_ops(ClusterSetup):
         error_sim[target_nodes.ip] = CouchbaseError(self.log, shell_conn[target_nodes.ip])
         error_sim[target_nodes.ip].create(CouchbaseError.KILL_MEMCACHED, bucket_name=bucket_big.name)
 
-        self.assertTrue(self.bucket_util._wait_warmup_completed([target_nodes], bucket_small, self.warmup_timeout) and
-                        (not(self.bucket_util._wait_warmup_completed([target_nodes], bucket_big, self.warmup_timeout)))
+        self.assertTrue(self.bucket_util._wait_warmup_completed([target_nodes], bucket_small) and
+                        (not(self.bucket_util._wait_warmup_completed([target_nodes], bucket_big)))
                         , "Bucket with less data not accessible when other bucket getting warmed up.")
 
         shell_conn[target_nodes.ip].disconnect()
