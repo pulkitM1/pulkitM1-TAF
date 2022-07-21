f2c0ab438efe4d8ebc6fb5f1efe0aa8bc315e793
pulkitM1 <pulkit.matta@couchbase.com>
2022-07-21 13:48:06 +0530

    commit

--- pytests/bucket_collections/bucket_templates/magma_dgm/5_percent_dgm/5_node_2_replica_magma_512.py
+++ pytests/bucket_collections/bucket_templates/magma_dgm/5_percent_dgm/5_node_2_replica_magma_512.py
@@ -6,7 +6,7 @@ spec = {
     MetaConstants.REMOVE_DEFAULT_COLLECTION: False,
     MetaConstants.CREATE_COLLECTIONS_USING_MANIFEST_IMPORT: True,
     MetaConstants.NUM_SCOPES_PER_BUCKET: 1,
-    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 100,
+    MetaConstants.NUM_COLLECTIONS_PER_SCOPE: 120,
     MetaConstants.NUM_ITEMS_PER_COLLECTION: 1,
 
     Bucket.bucketType: Bucket.Type.MEMBASE,
